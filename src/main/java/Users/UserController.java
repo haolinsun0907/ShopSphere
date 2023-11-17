0660326fbc8f3c1b2276af78b1808c4b166ac396
tysonlihaoyuan <hli165@uottawa.ca>
2023-11-17 17:37:29 -0600

    AddUserModel

---
+++ src/main/java/Users/UserController.java
@@ -0,0 +1,16 @@
+package Users;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/user")
+public class UserController {
+    @GetMapping
+    public List<User> getUser(){
+        return List.of(new User ());
+    }
+}
